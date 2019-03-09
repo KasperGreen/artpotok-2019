58c37bc7905421a2643b1bb7819a0a969c7e32ad
Kasper Green <kaspergreen@gmail.com>
2019-03-09 05:44:59 +0300

    emotion theming

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { hot } from 'react-hot-loader/root'
-import { css, Global } from '@emotion/core'
+import { Global } from '@emotion/core'
 import root_CSS from 'styles/root_CSS'
+import { ThemeProvider } from 'emotion-theming'
+import theme from 'styles/theme'
 
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <Global styles={root_CSS} />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p css={css`color: red;`}>
-            Поток Арт-Кемпинг!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Global styles={root_CSS} />
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Поток Арт-Кемпинг!
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </ThemeProvider>
     )
   }
 }
