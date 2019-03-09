54651af68a91e7a02e9db82dad6899ec393b1c70
Kasper Green <kaspergreen@gmail.com>
2019-03-09 06:48:30 +0300

    loading animation

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -6,29 +6,41 @@ import { Global } from '@emotion/core'
 import root_CSS from 'styles/root_CSS'
 import { ThemeProvider } from 'emotion-theming'
 import theme from 'styles/theme'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 class App extends Component {
   render () {
     return (
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <Global styles={root_CSS} />
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Поток Арт-Кемпинг!
-            </p>
-            <a
-              className="App-link"
-              href="https://artpotok.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              ArtPotok
-            </a>
-          </header>
-        </div>
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Route
+            render={(route_props) => {
+              console.log('%c →→→→ ', 'color: green', route_props, '← route_props | ')
+              return null
+            }}
+          />
+          <div className="App">
+            <Global styles={root_CSS} />
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Поток Арт-Кемпинг!
+              </p>
+              <a
+                className="App-link"
+                href="https://artpotok.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ArtPotok
+              </a>
+
+              <Link to={'/11'}>11</Link>
+              <Link to={'/222'}>222</Link>
+            </header>
+          </div>
+        </ThemeProvider>
+      </Router>
     )
   }
 }
