ab2769009e01cf5e450d1f85f162263a373bb6e9
Kasper Green <kaspergreen@gmail.com>
2019-03-10 05:21:16 +0300

    in pixels translate centring

--- src/containers/Landing/Logo/landing_page_logo_wrapper_square_landscape_CSS.js
+++ src/containers/Landing/Logo/landing_page_logo_wrapper_square_landscape_CSS.js
@@ -2,8 +2,9 @@ import { css } from '@emotion/core'
 
 export default css`
   position: absolute;
-  top: 50vh;
-  left: 100vw;
+
+  top: ${window.innerHeight * 0.5}px;
+  left: ${window.innerWidth}px;
   transform: translate(-90%, -50%);
 
 `
--- src/containers/Landing/Logo/landing_page_logo_wrapper_wide_landscape_CSS.js
+++ src/containers/Landing/Logo/landing_page_logo_wrapper_wide_landscape_CSS.js
@@ -2,8 +2,9 @@ import { css } from '@emotion/core'
 
 export default css`
   position: absolute;
-  top: 50vh;
-  left: 50vw;
+  
+  top: ${window.innerHeight * 0.5}px;
+  left: ${window.innerWidth * 0.5}px;
   transform: translate(-50%, -50%);
 
 `
