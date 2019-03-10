f6e2d26d91a0c356396cb9b93be090b1bc341010
Kasper Green <kaspergreen@gmail.com>
2019-03-10 20:29:31 +0300

    add balloon animation

--- src/containers/Landing/Baloon/landing_page_balloon_in_animation.js
+++ src/containers/Landing/Baloon/landing_page_balloon_in_animation.js
@@ -5,7 +5,7 @@ export default keyframes`
       transform: translate3d(2vw, -42vh, -120vw) scale(.2);
     }
     50% {
-      transform: translate3d(-2vw, -5vh, -100vw) scale(.5);
+      transform: translate3d(-1vw, -5vh, -100vw) scale(.5);
     }
     to {
       transform: translate3d(0, 0, 0);
